63b6993113ae37d7294af5be2024c99ff3ee87ff
Kai Takami <kaitakami@protonmail.com>
2023-12-19 21:49:06 -0600

    feat: Add website data file with social media links

---
+++ website-data.tsx
@@ -0,0 +1,18 @@
+import { LinkedinIcon, GithubIcon } from "lucide-react"
+
+const websiteData = {
+  "socials": [
+    {
+      "href": "https://linkedin.com/in/kaitakami",
+      "text": "LinkedIn",
+      "icon": LinkedinIcon,
+    },
+    {
+      "href": "https://github.com/kaitakami",
+      "text": "Github",
+      "icon": GithubIcon
+    }
+  ]
+}
+
+export default websiteData
