3193c186aca9e153da3a8ec05fe5941c655ac74e
Kai Takami <kaitakami@protonmail.com>
2023-12-22 13:27:22 -0600

    fix: update metadata image URLs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
   title: 'Kai Takami',
   description: 'Full-stack software engineer with 3+ years of experience focused on building user-centric products, delivering high-quality code, emphasizing reliable systems, and ensuring scalability.',
   openGraph: {
-    images: "/public/og.jpg",
+    images: "https://new-kai-takami.vercel.app/og.jpg",
     type: "website",
     description: "Software Engineer 3+ years of experience focused on building user-centric products, delivering high-quality code, emphasizing reliable systems, and ensuring scalability.",
     title: "Kai Takami"
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     title: 'Kai Takami',
     description: 'Software Engineer 3+ years of experience focused on building user-centric products, delivering high-quality code, emphasizing reliable systems, and ensuring scalability.',
     creator: '@kaitakami_',
-    images: "/public/og.jpg",
+    images: "https://new-kai-takami.vercel.app/og.jpg",
   },
 }
 
