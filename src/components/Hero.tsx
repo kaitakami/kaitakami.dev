de7aee1b138a88813d2a5846d757d4795092c82d
Kai Takami <kaitakami@protonmail.com>
2023-12-21 23:44:28 -0600

    feat: add Syne font and update Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,30 +1,40 @@
+import { Syne } from "next/font/google"
 import websiteData from "../../website-data"
 import Link from "next/link"
+
+const syneFont = Syne({
+  style: "normal",
+  subsets: ["latin"],
+  weight: ["700"]
+})
 
 const Hero = () => {
   return (
     <section className="h-[90vh] flex flex-col justify-center relative space-y-3">
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-7xl">
-        Kai<br />
-        Takami
-      </h1>
-      <p className="max-w-sm leading-5 [&:not(:first-child)]:mt-6 text-secondary text-pretty pb-16">
-        Full-stack software engineer with 3+ years of experience focused on building user-centric products, delivering high-quality code, emphasizing reliable systems, and ensuring scalability.
-      </p>
-      <div className="hidden sm:flex absolute right-0 flex-col rounded border-2 border-dark">
-        {websiteData.socials.map((social) => (
-          <Link
-            key={social.href}
-            href={social.href}
-            className="[&:not(:last-child)]:border-b-2 py-3 p-3 border-dark"
-            target="_blank"
-          >
-            <social.icon className="" />
-          </Link>
-        ))
-        }
+      <div className="relative">
+        <h1 className={`${syneFont.className} scroll-m-20 text-6xl font-extrabold tracking-tight lg:text-8xl`}>
+          Kai<br />
+          Takami
+        </h1>
+        <p className="max-w-sm leading-5 [&:not(:first-child)]:mt-6 text-secondary text-pretty pb-16">
+          Full-stack software engineer with 3+ years of experience focused on building user-centric products, delivering high-quality code, emphasizing reliable systems, and ensuring scalability.
+        </p>
+        <div className="sm:hidden flex rounded border-2 border-dark text-primary absolute right-0 -top-5">
+          {websiteData.socials.map((social) => (
+            <Link
+              key={social.href}
+              href={social.href}
+              className="[&:not(:last-child)]:border-r-2 py-3 p-3 border-dark"
+              target="_blank"
+            >
+              <social.icon />
+            </Link>
+          ))
+          }
+        </div>
+
       </div>
-      <div className="sm:hidden flex flex-col rounded border-2 border-dark">
+      <div className="hidden sm:flex absolute right-0 flex-col rounded border-2 border-dark text-secondary">
         {websiteData.socials.map((social) => (
           <Link
             key={social.href}
@@ -33,7 +43,6 @@ const Hero = () => {
             target="_blank"
           >
             <social.icon className="" />
-            {social.text}
           </Link>
         ))
         }
