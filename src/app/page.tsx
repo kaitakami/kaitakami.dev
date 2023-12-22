a1ccd94286b680a1391b3ded1868b889749152fa
Kai Takami <kaitakami@protonmail.com>
2023-12-22 13:24:22 -0600

    feat: add Blog and Footer components to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import Blog from '@/components/Blog'
 import Container from '@/components/Container'
+import Footer from '@/components/Footer'
 import Hero from '@/components/Hero'
 
 export default function Home() {
   return (
     <Container>
       <Hero />
+      <Blog />
+      <Footer />
     </Container>
   )
 }
