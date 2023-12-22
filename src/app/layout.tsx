7723424169d5a19315dc47c131e5d5c5c6678b24
Kai Takami <kaitakami@protonmail.com>
2023-12-22 13:24:15 -0600

    feat: update font and add Open Graph and Twitter card images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,29 @@
 import type { Metadata } from 'next'
-import { Young_Serif } from 'next/font/google'
+import { Syne } from "next/font/google"
 import './globals.css'
 
-const youngSerif = Young_Serif({
-  weight: ['400'],
-  subsets: ["latin"]
+const syneFont = Syne({
+  style: "normal",
+  subsets: ["latin"],
+  weight: ["700", "400"],
 })
 
 export const metadata: Metadata = {
   title: 'Kai Takami',
   description: 'Full-stack software engineer with 3+ years of experience focused on building user-centric products, delivering high-quality code, emphasizing reliable systems, and ensuring scalability.',
+  openGraph: {
+    images: "/public/og.jpg",
+    type: "website",
+    description: "Software Engineer 3+ years of experience focused on building user-centric products, delivering high-quality code, emphasizing reliable systems, and ensuring scalability.",
+    title: "Kai Takami"
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Kai Takami',
+    description: 'Software Engineer 3+ years of experience focused on building user-centric products, delivering high-quality code, emphasizing reliable systems, and ensuring scalability.',
+    creator: '@kaitakami_',
+    images: "/public/og.jpg",
+  },
 }
 
 export default function RootLayout({
@@ -19,7 +33,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${youngSerif.className} bg-gradient-to-br from-background-start to-background-end min-h-screen text-primary`}>{children}</body>
+      <body className={`${syneFont.className} bg-gradient-to-br from-background-start to-background-end min-h-screen text-primary`}>{children}</body>
     </html>
   )
 }
