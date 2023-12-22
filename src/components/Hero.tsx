65a09adf6762aeecccfd7e2598919189c2450eed
Kai Takami <kaitakami@protonmail.com>
2023-12-22 13:24:56 -0600

    feat: refactor font imports and update Hero component styles

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,30 +1,31 @@
-import { Syne } from "next/font/google"
 import websiteData from "../../website-data"
 import Link from "next/link"
+import { Young_Serif } from 'next/font/google'
 
-const syneFont = Syne({
-  style: "normal",
-  subsets: ["latin"],
-  weight: ["700"]
+const youngSerif = Young_Serif({
+  weight: ['400'],
+  subsets: ["latin"]
 })
+
 
 const Hero = () => {
   return (
-    <section className="h-[90vh] flex flex-col justify-center relative space-y-3">
+    <section className="h-[80vh] flex flex-col sm:justify-center relative space-y-3 sm:pt-0 pt-40">
       <div className="relative">
-        <h1 className={`${syneFont.className} scroll-m-20 text-6xl font-extrabold tracking-tight lg:text-8xl`}>
+        <h1 className={`${youngSerif.className} scroll-m-20 text-6xl tracking-tight lg:text-8xl`}>
           Kai<br />
           Takami
         </h1>
-        <p className="max-w-sm leading-5 [&:not(:first-child)]:mt-6 text-secondary text-pretty pb-16">
+        <p className={`max-w-sm leading-relaxed [&:not(:first-child)]:mt-6 text-secondary text-pretty pb-16 text-lg`}>
           Full-stack software engineer with 3+ years of experience focused on building user-centric products, delivering high-quality code, emphasizing reliable systems, and ensuring scalability.
         </p>
-        <div className="sm:hidden flex rounded border-2 border-dark text-primary absolute right-0 -top-5">
+        {/* MOBILE */}
+        <div className="sm:hidden flex text-secondary absolute right-0 -top-5">
           {websiteData.socials.map((social) => (
             <Link
               key={social.href}
               href={social.href}
-              className="[&:not(:last-child)]:border-r-2 py-3 p-3 border-dark"
+              className="[&:not(:last-child)]:border-r py-3 p-3 border-dark"
               target="_blank"
             >
               <social.icon />
@@ -32,9 +33,9 @@ const Hero = () => {
           ))
           }
         </div>
-
       </div>
-      <div className="hidden sm:flex absolute right-0 flex-col rounded border-2 border-dark text-secondary">
+      {/* OVER SM */}
+      <div className="hidden sm:flex absolute right-0 flex-col rounded border-2 border-dark text-secondary opacity-60">
         {websiteData.socials.map((social) => (
           <Link
             key={social.href}
@@ -42,7 +43,7 @@ const Hero = () => {
             className="[&:not(:last-child)]:border-b-2 py-3 p-3 border-dark"
             target="_blank"
           >
-            <social.icon className="" />
+            <social.icon />
           </Link>
         ))
         }
