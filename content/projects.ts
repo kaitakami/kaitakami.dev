b21e1be89a17ab6f0ee0e5121b14a8b34633b3f1
Kai Takami <kaitakami@protonmail.com>
2023-12-23 23:30:51 -0600

    feat: add projects and Projects component

---
+++ content/projects.ts
@@ -0,0 +1,41 @@
+import type { Project } from "@/components/Projects"
+
+export const projects: Project[] = [
+  {
+    title: "Kazeai",
+    description: "Improve your communication skills with straight-forward feedback",
+  },
+  {
+    title: "Kenso",
+    date: "2023-11-24",
+    description: "AI companion for teachers",
+    demo: "https://www.kenso.tech"
+  },
+  {
+    title: "Platzbot",
+    date: "2023-10-01",
+    description: "Your fitness Whatsapp bot",
+    demo: "https://www.youtube.com/watch?v=S-QlQtG2pU0&t=8s"
+  },
+  {
+    title: "Fingaku",
+    date: "2023-05-28",
+    description: "Track your student loans and finances with AI",
+    github: "https://github.com/kaitakami/fingaku",
+    demo: "https://fingaku.kaitakami.dev/"
+  },
+  {
+    title: "Enzan",
+    date: "2023-04-18",
+    description: "Collaborate with other developers and build projects together",
+    github: "https://github.com/kaitakami/enzan",
+    demo: "https://www.enzan.dev/"
+  },
+  {
+    title: "Learn React Hooks /w TypeScript",
+    date: "2023-02-25",
+    description: "Learn the basics of React Hooks with practical examples and TypeScript",
+    github: "https://github.com/kaitakami/learn-react-ts-hooks",
+    demo: "https://learn-react-hooks-easy.netlify.app/"
+  },
+]
