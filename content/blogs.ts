46e293ab1c5c3bfa4ad56733de93d83d3badb7d6
Kai Takami <kaitakami@protonmail.com>
2023-12-23 23:30:19 -0600

    feat: replace old blogs with new blogs

---
+++ content/blogs.ts
@@ -0,0 +1,20 @@
+import type { Blog } from "@/components/Blogs"
+
+export const blogs: Blog[] = [
+  {
+    title: "Andrej Karpathy - State of GPT",
+    date: "2023-06-04",
+    description: "Summary of the 2023 Microsoft Build Andrej Karpathy talk",
+    slug: "andrej-karpathy-state-of-gpt"
+  },
+  {
+    title: "How we built a Whatsapp Chatbot in 48 hours",
+    description: "And made our first sale in 24 hours",
+    slug: "how-we-built-a-whatsapp-chatbot-in-48-hours"
+  },
+  {
+    title: "Joining a B2B early-stage startup",
+    description: "Learnings and reflections from my first 6 months",
+    slug: "joining-a-b2b-early-stage-startup"
+  },
+]
