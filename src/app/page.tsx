846368982e8cbea08ada3b11520096166cea44b9
Kai Takami <kaitakami@protonmail.com>
2023-12-23 23:32:22 -0600

    feat: update imports and add Projects component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
-import Blog from '@/components/Blog'
+import Blogs from '@/components/Blogs'
 import Container from '@/components/Container'
 import Footer from '@/components/Footer'
 import Hero from '@/components/Hero'
+import Projects from '@/components/Projects'
 
 export default function Home() {
   return (
     <Container>
       <Hero />
-      <Blog />
+      <Blogs />
+      <Projects />
       <Footer />
     </Container>
   )
