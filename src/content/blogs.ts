448c0e0e646c2952cacc95fed1e2c9e5a3d3f004
kaitakami <kaitakami@protonmail.com>
2024-07-27 10:05:32 -0600

    feat: add new blog about boilerplate

--- src/content/blogs.ts
+++ src/content/blogs.ts
@@ -2,6 +2,12 @@ import type { Blog } from "@/components/Blogs"
 
 export const blogs: Blog[] = [
   {
+    title: "Why you should not buy Gravity",
+    date: "2024-07-27",
+    description: "A real and honest review of gravity boilerplate. And why we don't recommend it after months of using it.",
+    slug: "why-you-should-not-buy-gravity"
+  },
+  {
     title: "Andrej Karpathy - State of GPT",
     date: "2023-06-04",
     description: "Summary of the 2023 Microsoft Build Andrej Karpathy talk",
